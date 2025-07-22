c7a4832f05fa97fbb63f1af3c090efc4156955d3
UltharCat <heiyuelgy@qq.com>
2025-07-22 20:49:29 +0800

    add chat streaming functionality to OllamaAiController

--- RAG-Ai/src/main/java/com/ragai/controller/OllamaAiController.java
+++ RAG-Ai/src/main/java/com/ragai/controller/OllamaAiController.java
@@ -1,10 +1,12 @@
 package com.ragai.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/ollamaAi")
@@ -17,9 +19,38 @@ public class OllamaAiController {
 
     }
 
+    /**
+     * 单次响应对话
+     * @param prompt
+     * @return
+     */
     @GetMapping("/chat")
     public String chat(@RequestParam("prompt") String prompt) {
         return chatClient.prompt().user(prompt).call().content();
     }
+
+    /**
+     * MVC流式响应对话
+     * @param prompt
+     * @return
+     */
+    @GetMapping("/chat/stream")
+    public SseEmitter charStreamSse(@RequestParam("prompt") String prompt, HttpServletResponse response) {
+        response.setContentType("text/event-stream;charset=UTF-8");
+        SseEmitter emitter = new SseEmitter();
+        chatClient.prompt().user(prompt).stream().content().subscribe(
+                content -> {
+                    try {
+                        emitter.send(content);
+                    } catch (Exception e) {
+                        emitter.completeWithError(e);
+                    }
+                },
+                emitter::completeWithError,
+                emitter::complete
+        );
+        return emitter;
+    }
+
 
 }
