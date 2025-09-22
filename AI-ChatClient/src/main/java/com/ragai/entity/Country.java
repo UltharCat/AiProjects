86698d6891e7a688a98eaefe8cfcbced3e4cb00b
UltharCat <heiyuelgy@qq.com>
2025-09-22 15:13:07 +0800

    ref:项目结构优化及代码格式优化

--- AI-ChatClient/src/main/java/com/ragai/entity/Country.java
+++ AI-ChatClient/src/main/java/com/ragai/entity/Country.java
@@ -1,6 +1,4 @@
 package com.ragai.entity;
 
-import java.util.List;
-
 public record Country(String name, String capital) {
 }
--- AI-Ollama-Base/src/main/java/com/ragbase/service/OllamaBaseService.java
+++ AI-Ollama-Base/src/main/java/com/ragbase/service/OllamaBaseService.java
@@ -4,8 +4,8 @@ public interface OllamaBaseService {
 
     /**
      * 与Ollama进行对话
-     * @param prompt
-     * @return
+     * @param prompt 用户输入
+     * @return assistant 输出
      */
     String chatWithOllama(String prompt);
 
