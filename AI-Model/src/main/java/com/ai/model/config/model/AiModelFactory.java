1141a7c0af70d698da35e8cde98110da55e83924
UltharCat <heiyuelgy@qq.com>
2025-11-23 08:33:34 +0800

    ref:修正原本ai-model中的modelFactory的使用，其中的embeddingModel理解错误，不属于常规model

--- AI-Model/src/main/java/com/ai/model/config/model/AiModelFactory.java
+++ AI-Model/src/main/java/com/ai/model/config/model/AiModelFactory.java
@@ -12,9 +12,7 @@ public record AiModelFactory(Map<String, Model<?, ?>> allModels) {
     public Model<?, ?> getModel(String modelType) {
         String type = StringUtil.isNullOrEmpty(modelType) ? "chat" : modelType.trim();
         return switch (type) {
-            case "ollama-embedding" -> allModels.get("ollamaEmbeddingModel");
             case "openai-chat" -> allModels.get("openAiChatModel");
-            case "openai-embedding" -> allModels.get("openAiEmbeddingModel");
             case "openai-image" -> allModels.get("openAiImageModel");
             case "openai-audio-transcription" -> allModels.get("openAiAudioTranscriptionModel");
             case "openai-audio-speech" -> allModels.get("openAiAudioSpeechClient");
