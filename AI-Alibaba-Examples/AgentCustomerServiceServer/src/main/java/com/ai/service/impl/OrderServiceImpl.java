a77ecaadbd1d036853668deb85b6f362bd0498a9
UltharCat <UltharCat@noreply.gitcode.com>
2026-01-14 23:47:50 +0800

    ref:完善取消订单逻辑

--- AI-Alibaba-Examples/AgentCustomerServiceServer/src/main/java/com/ai/service/impl/OrderServiceImpl.java
+++ AI-Alibaba-Examples/AgentCustomerServiceServer/src/main/java/com/ai/service/impl/OrderServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -60,20 +61,16 @@ public class OrderServiceImpl implements OrderService {
         // 重设订单商品库存信息
         List<TOrderItem> items = orderItemRepository.findByOrderId(orderId);
         for (TOrderItem item : items) {
-            // 获取产品id
-            Long productId = item.getProduct().getId();
             // 获取购买数量
             Integer qty = item.getQuantity();
-            List<TInventory> inventories = inventoryRepository.findByProductId(productId);
-            if (inventories != null && !inventories.isEmpty()) {
-                // 重设库存资料（简单的将第一个仓库的库存增加，其实应该下单商品关联库存和商品）
-                TInventory inv = inventories.get(0);
-                inv.setQuantity(inv.getQuantity() + (qty == null ? 0 : qty));
-                inventoryRepository.save(inv);
+            // 获取库存信息
+            TInventory inventory = item.getInventory();
+            if (Objects.nonNull(inventory)) {
+                // 重设库存资料
+                inventory.setQuantity(inventory.getQuantity() + (qty == null ? 0 : qty));
+                inventoryRepository.save(inventory);
             }
         }
-
-
         return true;
     }
 
