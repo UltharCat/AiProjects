16629899741b7b7adeb0211a7e5aa2faaa539fe8
UltharCat <UltharCat@noreply.gitcode.com>
2026-01-20 02:26:07 +0800

    ref: 重命名AgentDairyClient和AgentDairyServer相关类及文件

---
+++ LangChain4j-Examples/AgentDiaryClient/src/main/java/com/ai/config/AgentServiceFactory.java
@@ -0,0 +1,19 @@
+package com.ai.config;
+
+import com.ai.agent.service.AgentService;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.service.AiServices;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AgentServiceFactory {
+
+    @Bean
+    public AgentService chatAgentService(QwenChatModel qwenChatModel) {
+        return AiServices.builder(AgentService.class)
+                .chatModel(qwenChatModel)
+                .build();
+    }
+
+}
---
+++ LangChain4j-Examples/AgentDiaryServer/src/main/java/com/ai/AgentDiaryServerApplication.java
@@ -0,0 +1,13 @@
+package com.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentDiaryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentDiaryServerApplication.class, args);
+    }
+
+}
